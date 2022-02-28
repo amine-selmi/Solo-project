9e880ee2aeb5fbaf915d591ea5210c35dfa0f007
amine <amine.selmi@esprit.tn>
2022-02-28 20:32:09 +0100

    flippe cards

--- main.js
+++ main.js
@@ -8,35 +8,44 @@ $(function(){
            
             this.shuffle();
             $('.start-game').on('click', function(e) {
-				app.gatherData(e);
+                $('.content').removeClass('hide')
+                $('.menu').fadeOut();
+                app.gatherData(e);
 			});
+				
             
 
         },
         shuffle: function(){
             var random=0;
-            var permut=0;
+            var temp=0;
             console.log(this.cards);
             for(var i=0;i<this.cards.length;i++){
                 random = Math.round(Math.random()*i)
                 temp = this.cards[i]
                 this.cards[i]= this.cards[random]
                 this.cards[random]= temp
-                $('.cards').append('<div class="card unmatched"></div>');
+                $('.cards').append(' <div class="card unmatched"></div>');
+                
+                
             }
+            // console.log($('.cards'));
+            // 
             $('.card').each(function(index){
 				$(this).attr('data-card', app.cards[index]);
+                
 			});
             console.log(this.cards);
         },
         gatherData: function(e){
+            console.log(e);
             e.preventDefault();
 			if($('.player-name').val() !== '') {
 				app.player = $('.player-name').val();
 				app.event();
 				app.startTime = new Date();
-				console.log(app.startTime);
-				console.log(app.player);
+                    // console.log(app.startTime);
+                    // console.log(app.player);
 			} else {
 				$('input[type=text]').addClass('shake');
 				setTimeout(function() { $('input[type=text]').removeClass('shake'); }, 500);
@@ -45,18 +54,19 @@ $(function(){
 			$('.menu').fadeOut();
         },
         event: function() {
-			$('.card').on('click', function() {
                 $('.card').on('click',function(){
-                    $(this).html("<p class = 'centerr'>"+$(this).attr('data-card')+"</p>" )
-                })
-                $('centerr').show()
-				app.flipCard($(this));
-			});
+                    $(this).addClass("selected");
+                    // $(this).addClass("flip");
+                   this.classList.toggle('flip')
+                    // $(this).html("<p class = 'centerr'>"+$(this).attr('data-card')+"</p>" )
+                    $(this).append('<img class="back-face" src="../assets/'+$(this).attr('data-card')+'.png"></img>');
+                    $('.back-face').toggle();
+				    app.flipCard(this);
+                    //console.log(this);
+                })			
 		},
         flipCard: function(card){
-            console.log($('.card'));
-            card.addClass("selected");
-
+            $('.back-face').toggle();
             if($('.selected').length === 2) {
 				app.checkMatch();
 			}
@@ -66,7 +76,7 @@ $(function(){
 				setTimeout(function() {
 					$('.selected').each(function() {
                         app.counter-=1
-                        console.log(app.counter)
+                        // console.log(app.counter)
 						$(this).removeClass('unmatched selected').css({'background':''}).animate({ opacity: 0 },{duration: 1000});
 						app.checkWin()
 					});
@@ -74,14 +84,15 @@ $(function(){
 			} else {
 				setTimeout(function(){
 					$('.selected').each(function() {
-						$(this).html('').removeClass('selected').css({'background':''});
+						$(this).html('').removeClass('selected').css({'background':''}).removeClass('flip');
+                        $('img').remove();
 					});
 				}, 1000);
                 app.checkWin()
 			}
         },
         checkWin: function() {
-            console.log($('.selected').length)
+            // console.log($('.selected').length)
             if(app.counter ===0 ){
 				$('.results').show();
 				$('.results').css('display','block !important')
@@ -94,9 +105,9 @@ $(function(){
 
 
     }
-    $('.start-game').on('click',function(){
-        $('.content').removeClass('hide')
-        $('.menu').fadeOut();
-    })
+    // $('.start-game').on('click',function(){
+    //     $('.content').removeClass('hide')
+    //     $('.menu').fadeOut();
+    // })
     app.init();
 });
